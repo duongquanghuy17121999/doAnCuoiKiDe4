7055007155a64ad6ea1d59ff42f8d68186f38d70
duongquanghuy17121999 <51703097@student.tdtu.edu.vn>
2020-03-31 23:13:30 +0700

    in hd

---
+++ InHoaDon.cs
@@ -0,0 +1,36 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class InHoaDon : Form
+    {
+        public InHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void InHoaDon_Load(object sender, EventArgs e)
+        {
+            ReportDocument objrep = new rpHoaDon();
+            SqlConnection mycon = new SqlConnection(@"Data Source=LAPTOP-DSA2JMNK\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+            mycon.Open();
+            SqlDataAdapter data = new SqlDataAdapter(Program.sql,
+            mycon);
+            DataSet ds = new DataSet();
+            data.Fill(ds, Program.sql);
+            objrep.SetDataSource(ds.Tables[0]);
+            crystalReportViewer1.ReportSource = objrep;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
