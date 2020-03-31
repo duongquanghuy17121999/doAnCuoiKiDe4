da667ba2970536a5eab99aafec3387e6ef487793
duongquanghuy17121999 <51703097@student.tdtu.edu.vn>
2020-03-31 23:04:37 +0700

    progarm

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    static class Program
+    {
+        public static string sql = "";
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
